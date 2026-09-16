42e7a13c179a216a4f2bc4e035aabab330affbac
Harkonnen <gunjotikaraditya@gmail.com>
2026-09-16 07:21:01 +0530

    Search

---
+++ ExponentialSearch.py
@@ -0,0 +1,19 @@
+def exponential_search(arr, target):
+    n = len(arr)
+    if n == 0:
+        return -1
+    if arr[0] == target:
+        return 0
+    i = 1
+    while i < n and arr[i] <= target:
+        i *= 2
+    low, high = i // 2, min(i, n - 1)
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
